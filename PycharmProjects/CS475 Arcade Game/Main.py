6e0116e38822f061df7f030dc08c8656373ebcc3
michael <macbad2012@gmail.com>
2017-02-07 15:54:31 -0500

    Started Arcade Game

--- PycharmProjects/CS475 Arcade Game/Main.py
+++ PycharmProjects/CS475 Arcade Game/Main.py
@@ -15,7 +15,8 @@ pygame.init()
 pygame.mixer.init()
 pygame.display.set_mode((650, 850))
 interval = 1
-songList = {"sound/Song1.wav", "sound/Song2.wav", "sound/Song3.wav"}
+songList = ["sound/Song1.wav", "sound/Song2.wav", "sound/Song3.wav"]
+isPlaying = True
 
 #play the theme song while on the main menu
 themeSong = pygame.mixer.Sound("sound/Theme.wav")
@@ -29,19 +30,32 @@ while gameStart == False:
         #give player option to quit game before it begins
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             sys.exit()
-
         #press enter to start the game
         if event.type == pygame.KEYDOWN and (event.key == pygame.K_KP_ENTER or event.key == pygame.K_RETURN):
-            print("hello1")
             gameStart = True
             break
+        if event.type == pygame.KEYDOWN and (event.key == pygame.K_MINUS or event.key == pygame.K_KP_MINUS):  # volume down
+            if channel1.get_volume() > 0:
+                channel1.set_volume(channel1.get_volume() - .1)
+        if event.type == pygame.KEYDOWN and (event.key == pygame.K_EQUALS or event.key == pygame.K_KP_PLUS):  # volume up
+            if channel1.get_volume() < 1:
+                channel1.set_volume(channel1.get_volume() + .1)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_m:
+            if isPlaying:
+                isPlaying = False
+                channel1.pause()
+            else:
+                isPlaying = True
+                channel1.unpause()
 #
 #set up the game
 #
 
 #set the first song
+channel1.stop() #stop the theme song
 currentSong = pygame.mixer.Sound(songList[interval])
 channel1 = currentSong.play()
+isPlaying = True
 
 #game controls
 while gameEnd == False:
@@ -65,10 +79,18 @@ while gameEnd == False:
             if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT: #hyperdrive (movement speed increase)
                 print("p")
             if event.key == pygame.K_MINUS or event.key == pygame.K_KP_MINUS: #volume down
-                if channel1.get_volume > 0
-                    channel1.get_sound().set_volume(channel1.get_volume - .1)
+                if channel1.get_volume() > 0:
+                    channel1.set_volume(channel1.get_volume() - .1)
             if event.key == pygame.K_EQUALS or event.key == pygame.K_KP_PLUS: #volume up
-                if channel1.get_volume < 1
-                    channel1.get_sound().set_volume(channel1.get_volume + .1)
+                if channel1.get_volume() < 1:
+                    channel1.set_volume(channel1.get_volume() + .1)
+            if event.key == pygame.K_m:
+                if isPlaying:
+                    isPlaying = False
+                    channel1.pause()
+                else:
+                    isPlaying = True
+                    channel1.unpause()
+
 
 
