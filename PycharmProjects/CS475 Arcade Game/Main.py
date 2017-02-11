21d29e328640d23c12b03a2bedb085af091cdf05
michael <macbad2012@gmail.com>
2017-02-11 00:07:01 -0500

    Added the enemy ship sprite. It has a good center gun to shoot at the player with. Also added functionality for the music to change for each "wave". Once enemy shooting mechanics are in, it should do fine. Then, I need to add collision detection to everything.

--- PycharmProjects/CS475 Arcade Game/Main.py
+++ PycharmProjects/CS475 Arcade Game/Main.py
@@ -9,7 +9,14 @@
 #imports
 import pygame
 import sys
+import json
 from Sprite import Sprite
+
+#grab the data from the song text files (want to front-load this)
+f = open('sound/Song1.txt', 'r')
+THING = f.read()
+parsed_json = json.loads(THING)
+f.close()
 
 #set up pyGame
 pygame.init()
@@ -22,6 +29,9 @@ screenW = 650
 screenH = 850
 screen = pygame.display.set_mode((screenW, screenH))
 pygame.display.set_caption("Rhythm Pilot")
+
+#some variables
+isHyper = False
 
 #some song variables
 interval = 0
@@ -39,6 +49,9 @@ gameEnd = False #boolean to check if player has reached the win condition
 spriteList = pygame.sprite.Group()
 player = Sprite("sprite/PlayerShip.png", .5 * screenW - 40, .75 * screenH, 80, 80)
 
+#setup enemy sprite
+enemy = Sprite("sprite/EnemyShip.png", .5 * screenW - ((screenW)/2), 0, screenW, 250)
+
 #setup other sprites
 background = Sprite("sprite/Background.png", 0, 0, screenW, screenH)
 menuBackground = Sprite("sprite/MainMenu.png", 0, 0, screenW, screenH)
@@ -53,14 +66,12 @@ title = gameFont.render(titleText, True, (255, 255, 255))
 
 gameInsP1 = "Use W,A,S,D to move. You can also shoot with SPACE."
 gameInsP2 = "You can increase your movement speed with SHIFT."
-gameInsP2p2 = "(This is mainly useful for moving in straight lines though)"
 gameInsP3 = "Use + or - to control volume. M will mute the music."
 gameInsP4 = "Press ENTER or RETURN to begin."
 gameInsP4p2 = "(Use the first few seconds to get use to the controls)"
 
 ins1 = insFont.render(gameInsP1, True, (255, 255, 255))
 ins2 = insFont.render(gameInsP2, True, (255, 255, 255))
-ins2p2 = p2Font.render(gameInsP2p2, True, (255, 255, 255))
 ins3 = insFont.render(gameInsP3, True, (255, 255, 255))
 ins4 = insFont.render(gameInsP4, True, (255, 255, 255))
 ins4p2 = p2Font.render(gameInsP4p2, True, (255, 255, 255))
@@ -109,9 +120,8 @@ while gameStart == False:
     screen.blit(menuBackground.image, menuBackground.rect)
     screen.blit(title, (.5 * screenW - (gameFont.size(titleText)[0] / 2), 10))
     screen.blit(ins1, (.5 * screenW - (insFont.size(gameInsP1)[0] / 2), 50))
-    screen.blit(ins2, (.5 * screenW - (insFont.size(gameInsP2)[0] / 2), 50 + gameFont.size(gameInsP1)[1] * .8))
-    screen.blit(ins2p2, (.5 * screenW - (p2Font.size(gameInsP2p2)[0] / 2), 50 + gameFont.size(gameInsP1)[1] * 1.4))
-    screen.blit(ins3, (.5 * screenW - (insFont.size(gameInsP3)[0] / 2), 50 + gameFont.size(gameInsP1)[1] * 2))
+    screen.blit(ins2, (.5 * screenW - (insFont.size(gameInsP2)[0] / 2), 50 + gameFont.size(gameInsP1)[1] * .7))
+    screen.blit(ins3, (.5 * screenW - (insFont.size(gameInsP3)[0] / 2), 50 + gameFont.size(gameInsP1)[1] * 1.4))
     screen.blit(ins4, (.5 * screenW - (insFont.size(gameInsP4)[0] / 2), screenH - 100))
     screen.blit(ins4p2, (.5 * screenW - (p2Font.size(gameInsP4p2)[0] / 2), screenH - 50))
     # Refresh Screen
@@ -124,12 +134,13 @@ while gameStart == False:
 #set the first song
 pygame.mixer.music.stop() #stop the theme song
 pygame.mixer.music.load(songList[interval])
-pygame.mixer.music.play(-1)
+pygame.mixer.music.play() #no looping
 pygame.mixer.music.set_volume(.5)
 isPlaying = True
 
 #add appropriate sprites to the list
 spriteList.add(player)
+spriteList.add(enemy)
 
 #The game!
 while gameEnd == False:
@@ -152,7 +163,7 @@ while gameEnd == False:
                     player.rSpeed = player.speed
                 if event.key == pygame.K_SPACE: #shoot
                     #create a bullet if able
-                    if bulletCooldown >= 50:
+                    if bulletCooldown >= 30:
                         bulletCounter = bulletCounter + 1
                         if bulletCounter > len(bulletPool) - 1: #if at the end of pool, reset the index
                             bulletCounter = 0
@@ -164,10 +175,7 @@ while gameEnd == False:
                         spriteList.add(bullet)
                         bulletCooldown = 0
                 if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT: #hyperdrive (movement speed increase)
-                    player.uSpeed = player.uSpeed * 2
-                    player.lSpeed = player.lSpeed * 2
-                    player.dSpeed = player.dSpeed * 2
-                    player.rSpeed = player.rSpeed * 2
+                    isHyper = True
                 if event.key == pygame.K_MINUS or event.key == pygame.K_KP_MINUS: #volume down
                     if pygame.mixer.music.get_volume() > 0:
                         pygame.mixer.music.set_volume(pygame.mixer.music.get_volume() - .1)
@@ -192,7 +200,7 @@ while gameEnd == False:
                 if event.key == pygame.K_d:  # move right
                     player.rSpeed = 0
                 if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:  # hyperdrive (movement speed increase)
-                    player.speed = 5
+                    isHyper = False
 
         bulletCooldown = bulletCooldown + 1
 
@@ -201,10 +209,14 @@ while gameEnd == False:
         for sprite in spritesToModify:
             #update positions (using pixel-perfect movement)
             if sprite.rect.bottom < screenH and sprite.rect.top > 0 and sprite.rect.right < screenW and sprite.rect.left > 0:
-                sprite.rect.y = sprite.rect.y + sprite.uSpeed + sprite.dSpeed
-                sprite.rect.x = sprite.rect.x + sprite.lSpeed + sprite.rSpeed
+                if isHyper == False:
+                    sprite.rect.y = sprite.rect.y + sprite.uSpeed + sprite.dSpeed
+                    sprite.rect.x = sprite.rect.x + sprite.lSpeed + sprite.rSpeed
+                else:
+                    sprite.rect.y = sprite.rect.y + ((sprite.uSpeed + sprite.dSpeed)*2)
+                    sprite.rect.x = sprite.rect.x + ((sprite.lSpeed + sprite.rSpeed)*2)
 
-            #player position adjustments
+            #position adjustments
             while (sprite.rect.bottom >= screenH):
                 if sprite.destructable == True:  # if the obj is destructible and it hits a wall, get rid of it
                     spriteList.remove(sprite)
@@ -221,6 +233,14 @@ while gameEnd == False:
                 if sprite.destructable == True:  # if the obj is destructible and it hits a wall, get rid of it
                     spriteList.remove(sprite)
                 sprite.rect.x = sprite.rect.x + 1
+
+        if pygame.mixer.music.get_busy() == False:
+            interval = interval + 1
+
+            if interval < 4:
+                pygame.mixer.music.load(songList[interval])
+                pygame.mixer.music.play()  # no looping
+                pygame.mixer.music.set_volume(.5)
 
         #update screen
         spriteList.update()
